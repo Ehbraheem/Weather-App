30c11d9a280c594b0de86674ea1745132c9a45a3
Ehbraheem <ehbraheem@gmail.com>
2017-03-21 16:07:50 +0100

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@
  * Created by Ehbraheem on 21/03/2017.
  */
 
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + "/app/index.html",
+    filename:   "index.html",
+    inject:     "body"
+});
+
 module.exports = {
     entry : [
         './app/index.js'
@@ -9,12 +16,12 @@ module.exports = {
     output : {
         path : __dirname + '/dist',
         filename : "index_bundle.js"
-    }
-
+    },
     module : {
         loaders : [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
-    }
+    },
+    plugins: [HtmlWebpackPluginConfig]
 
 }

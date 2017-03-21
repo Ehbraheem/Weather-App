466abbfd05ff45672aa11906ca531409c0229bbd
Ehbraheem <ehbraheem@gmail.com>
2017-03-21 16:20:36 +0100

    Testing babel

--- app/index.js
+++ app/index.js
@@ -1,3 +1,26 @@
 /**
  * Created by Ehbraheem on 21/03/2017.
  */
+
+(function () {
+
+    "use strict";
+
+    var React      = require('react'),
+        ReactDom   = require('react-dom');
+
+    var HelloWorld = React.createClass({
+        render: function () {
+            return (
+                <div>Hello World</div>
+            )
+        }
+    });
+
+    ReactDom.render(
+        <HelloWorld />,
+
+        document.getElementById('app')
+    );
+
+})();

59472dfeb82359abd9342db8d47fa232c7e5eea8
Ehbraheem <ehbraheem@gmail.com>
2017-03-22 11:37:53 +0100

    Added Main and Home component

---
+++ components/Home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ehbraheem on 22/03/2017.
+ */
+
+(function () {
+
+    "use strict";
+
+    var React = require('react');
+
+    var Home = React.createClass({
+        render: () => (<div>Hello From Home</div>)
+    });
+
+    module.exports = Home;
+})();
---
+++ components/Main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Ehbraheem on 22/03/2017.
+ */
+
+(function () {
+
+    "use strict";
+
+    var React = require('react');
+
+    var Main = React.createClass({
+        render: () => ( <div> Hello from Main! {this.props.children} </div>)
+    })
+})();

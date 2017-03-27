647ae8f36ff20661749b697af0aa4ecd409fb1f4
Ehbraheem <ehbraheem@gmail.com>
2017-03-27 11:18:15 +0100

    Creat helper object

---
+++ app/utils/data.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Ehbraheem on 27/03/2017.
+ */
+
+(function () {
+
+    "use strict";
+
+    var helpers = {
+
+        getWeather: function () {
+
+        },
+
+    }
+
+    module.exports = helpers;
+})();
